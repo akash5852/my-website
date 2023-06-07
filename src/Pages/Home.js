41ab00fd22ef4d98d655273b4caf753191efc4be
Akash Sharma <shara98@mcmaster.ca>
2023-06-07 19:26:33 -0400

    peace

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,13 +6,15 @@ import { FaEnvelope } from 'react-icons/fa';
 import { FaInstagramSquare } from 'react-icons/fa';
 import { BsFillFileEarmarkPersonFill } from 'react-icons/bs'
 
+
+
 const Home = () => {
     const experiences = ['Java', 'Python', 'JavaScript', 'C', 'C++', 'HTML/CSS', 'Node.js', 'React', 'MongoDB', 'Express.js', 'Google Cloud Platform', 'PostgresSQL', 'Docker', 'Kubernetes'];
     const listItems = experiences.map((myList) => {
         return <li>&ensp; <Emoji symbol="•" label="star" /> &nbsp;{myList}</li>;
     });
   
-    const hobbies = [`Cruise on my Penny Skateboard`, 'Watch movies and TV shows', 'Work out and try to achieve my dream physique', 'Listen to lots of music', 'Play video games such as Valorant with friends'];
+    const hobbies = [`lifting weights 🏋️‍♂️`, 'watching movies and videos 🍿', 'hanging out with friends 🎾'];
     const listHobbies = hobbies.map((hobbies) => {
         return <li>&ensp; <Emoji symbol="•" label="star" /> &nbsp;{hobbies}</li>;
     });
@@ -23,7 +25,7 @@ const Home = () => {
                 <div className="display flex align-middle justify-center" id="pic-and-title">
                     <img className="w-[11rem] rounded-full " src="https://chandra.harvard.edu/photo/2011/a2744/a2744.jpg" alt="pic of me" />
                 </div>
-                <h1 className="tall:text-[2.3em] pt-4 pb-4 text-center text-3xl font-semibold"><Emoji symbol="👋" label="wave" /> Hey, I'm Akash <br/>Welcome to my website! </h1>
+                <h1 className="tall:text-[2.3em] pt-4 pb-4 text-center text-3xl font-semibold"><Emoji symbol="👋" label="wave" /> Hey, I'm Akash <br/>Welcome to my website!</h1>
                 <div className='pl-5 pr-5' id="main-section">
                     <div className='text-[1.5em] text display flex align-middle justify-center' id="icons">
                         <a rel="author" id = 'github' href="https://github.com/akash5852"><FaGithub className='hover:text-sky-500 ease-linear transition duration-300' /></a>
@@ -41,6 +43,13 @@ const Home = () => {
                         <p className="text-left">
                             I'm a third year Computer Engineering student at McMaster University with a passion for developing software that improves lives.
                         </p>
+
+                        <p className="text-left pt-5">
+                            In my free time you can catch me:
+                            <ul>
+                                {listHobbies}
+                            </ul>
+                        </p>
                         <p className="text-left pt-5">
                             I'm currently exploring the world of full stack development, and have experience in:<br></br>
                             <ul className="display inline-block">
@@ -54,12 +63,6 @@ const Home = () => {
                             </ul>
                             <ul className="lg:pl-3 display inline-block">
                                 {listItems.slice(9, 12)}
-                            </ul>
-                        </p>
-                        <p className="text-left pt-5">
-                            In my free time I love to:
-                            <ul>
-                                {listHobbies}
                             </ul>
                         </p>
                     </div>
